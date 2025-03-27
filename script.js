d01a32c821437edef1d7121746e036d06e0af41e
leith.alshalbi <majidleithalshalabi@gmail.com>
2025-03-27 11:43:21 +0100

    added front and basic back end

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 document.getElementById('upload-form').addEventListener('submit', function (e) {
     e.preventDefault();
-    const fileInput = document.getElementById('csvFile');
+    const fileInput = document.getElementById('xlsxFile');
     const file = fileInput.files[0];
 
     if (file) {
         const reader = new FileReader();
         reader.onload = function (event) {
-            const csvData = event.target.result;
+            const data = new Uint8Array(event.target.result);
+            const workbook = XLSX.read(data, { type: 'array' });
+            const firstSheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[firstSheetName];
+            const csvData = XLSX.utils.sheet_to_csv(worksheet);
             const formattedText = convertCSVToText(csvData);
             document.getElementById('formatted-text').textContent = formattedText;
         };
-        reader.readAsText(file);
+        reader.readAsArrayBuffer(file);
     } else {
-        alert('Please upload a CSV file.');
+        alert('Please upload an XLSX file.');
     }
 });
 
